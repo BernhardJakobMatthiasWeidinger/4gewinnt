3842fa6a87c4c11c415c2d8b7f0bdfafe4f73e66
Bernhard Weidinger <weidinger.bernhard@student.htlwrn.ac.at>
2020-04-22 22:03:23 +0200

    Server sends image successfully to client

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
@@ -12,7 +13,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	console.log('user connected');
+	console.log("user connect");
+	
+    var titleScreen = fs.readFileSync('./images/title.jpg', 'base64');
+	io.emit('init', titleScreen);
 	
 	socket.on('hello', function(msg) {
 		console.log(msg);
